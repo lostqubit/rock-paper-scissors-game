e536f2cc0515bd4672ed75dd7a9de9002a77b2bf
lostqubit <sg_suryaprakash@yahoo.com>
2024-04-29 12:17:04 +0530

    add random computer choice generator

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("test");
+function getComputerChoice(){
+    let choice = Math.floor(Math.random()*3);
+    if(choice===0) return "rock";
+    else if(choice===1) return "paper";
+    else return "scissors";
+}
