fdf1e06072dfaeb94a3a627db5d9bd4fa7d79b80
lostqubit <sg_suryaprakash@yahoo.com>
2024-04-29 13:06:06 +0530

    add logic for one round

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let choice = prompt("Enter your move:");
+    choice = choice.toLowerCase();
     while(choice!=="rock" && choice!=="paper" && choice!="scissors"){
         alert("Please Enter a valid move");
         choice = prompt("Enter your move:");
@@ -14,3 +15,30 @@ function getHumanChoice(){
 
     return choice;
 }
+
+function playRound(humanChoice,computerChoice){
+    if(humanChoice===computerChoice){
+        alert(`Tie! Computer chose ${computerChoice[0].toUpperCase()+computerChoice.slice(1)}.`);
+        return 0;
+    }
+    else if((humanChoice==="rock" && computerChoice==="scissors") || (humanChoice==="scissors" && computerChoice==="paper") || (humanChoice==="paper" && computerChoice==="rock")){
+        alert(`You win! Computer chose ${computerChoice[0].toUpperCase()+computerChoice.slice(1)}. ${humanChoice[0].toUpperCase() + humanChoice.slice(1)} beats ${computerChoice[0].toUpperCase()+computerChoice.slice(1)}.`);
+        return 1;
+    }
+    else{
+        alert(`You Lose! Computer chose ${computerChoice[0].toUpperCase()+computerChoice.slice(1)}. ${computerChoice[0].toUpperCase()+computerChoice.slice(1)} beats ${humanChoice[0].toUpperCase() + humanChoice.slice(1)}.`);
+        return -1;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+let humanMove = getHumanChoice();
+let computerMove = getComputerChoice();
+
+let res = playRound(humanMove,computerMove);
+
+humanScore+= res;
+computerScore-= res;
+
