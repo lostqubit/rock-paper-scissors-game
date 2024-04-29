6ba4e967e2446e69c9b1e829f07b911890af786b
lostqubit <sg_suryaprakash@yahoo.com>
2024-04-29 12:25:17 +0530

    add user input

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function getComputerChoice(){
     else if(choice===1) return "paper";
     else return "scissors";
 }
+
+function getHumanChoice(){
+    let choice = prompt("Enter your move:");
+    while(choice!=="rock" && choice!=="paper" && choice!="scissors"){
+        alert("Please Enter a valid move");
+        choice = prompt("Enter your move:");
+    }
+
+    return choice;
+}
