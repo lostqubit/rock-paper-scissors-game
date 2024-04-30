dd08c5436608cba3ebb915711c8efcb69376e51a
lostqubit <sg_suryaprakash@yahoo.com>
2024-04-30 16:14:48 +0530

    add interactive image for each player and compute move

--- script.js
+++ script.js
@@ -15,10 +15,30 @@ const playRound = (event) => {
     const playerChoice = event.target.id;
     const computerChoice = getComputerChoice();
 
-    playerMove.textContent = " " + playerChoice;
-    computerMove.textContent = " " + computerChoice;
-
     const outcome = computeRoundResult(playerChoice,computerChoice);
+
+    if(lastMovePlayer && lastMoveComputer){
+        playerMoveImg.src = `media/${playerChoice.toLowerCase()}.png`;
+        playerMoveImg.alt = playerChoice;
+        computerMoveImg.src = `media/${computerChoice.toLowerCase()}.png`;
+        computerMoveImg.alt = computerChoice;
+    }
+    else{
+        console.log("test");
+        for(let image of document.querySelectorAll(".image-container")){
+            image.textContent = "";
+        }
+        const playerMoveImg = document.createElement("img");
+        const computerMoveImg = document.createElement("img");
+
+        playerMoveImg.src = `media/${playerChoice.toLowerCase()}.png`;
+        playerMoveImg.alt = playerChoice;
+        computerMoveImg.src = `media/${computerChoice.toLowerCase()}.png`;
+        computerMoveImg.alt = computerChoice;
+
+        playerMove.append(playerMoveImg);
+        computerMove.append(computerMoveImg);
+    }
 
     if(outcome==0) return;
     else if(outcome===1){
@@ -29,11 +49,14 @@ const playRound = (event) => {
     }
 }
 
+let lastMovePlayer;
+let lastMoveComputer;
+
 const buttons = document.querySelectorAll("button");
-const playerMove = document.querySelector("#pMove");
-const computerMove = document.querySelector("#cMove");
 const playerScore = document.querySelector("#pScore");
 const computerScore = document.querySelector("#cScore");
+const playerMove = document.querySelector("#pMove");
+const computerMove = document.querySelector("#cMove");
 
 for(let button of buttons){
     button.addEventListener("click",playRound);
